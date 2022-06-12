53a642f51c97b0d31923acc94489c466e57ed934
heannnn <jse033101@gmail.com>
2022-06-13 00:06:08 +0900

    todolist 초기 세팅

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+export const DarkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#9c88ff",
+  cardBgColor: "transparent",
+};
