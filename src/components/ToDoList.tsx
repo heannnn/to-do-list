11b15606f3e01c4bbe4ed372f8ca6a0de440e42a
heannnn <jse033101@gmail.com>
2022-06-15 21:25:01 +0900

    category 변경

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -3,9 +3,43 @@ import { toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
+// [
+//   {
+//       "text": "5",
+//       "id": 1655294781298,
+//       "category": "TO_DO"
+//   },
+//   {
+//       "text": "4",
+//       "id": 1655294780290,
+//       "category": "TO_DO"
+//   },
+//   {
+//       "text": "3",
+//       "id": 1655294779978,
+//       "category": "TO_DO"
+//   },
+//   {
+//       "text": "2",
+//       "id": 1655294779610,
+//       "category": "TO_DO"
+//   },
+//   {
+//       "text": "1",
+//       "id": 1655294342387,
+//       "category": "TO_DO"
+//   },
+//   {
+//       "text": "11",
+//       "id": 1655294116771,
+//       "category": "TO_DO"
+//   }
+// ]
+
+// 1) find to do based on id [2]
+
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
-
   console.log(toDos);
   return (
     <div>
